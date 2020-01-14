2221487af7c70dc66123f707e2717ffcc094be67
David Holtkamp <david.hholtkamp@gmail.com>
2020-01-14 10:10:31 -0700

    Write tests for vendors that sell and functionality to pass iteration 2

--- lib/market.rb
+++ lib/market.rb
@@ -12,7 +12,19 @@ class Market
 
   def vendor_names
     @vendors.map do |vendor|
-      vendor.name 
+      vendor.name
     end
+  end
+
+  def vendors_that_sell(item_name)
+    vendors_that_sell = []
+    @vendors.map do |vendor|
+      vendor.check_inventory.find_all do |item|
+        if item.name == item_name
+          vendors_that_sell << vendor
+        end
+      end
+    end
+    vendors_that_sell
   end
 end
